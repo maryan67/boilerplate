df08408c0f783cc58888fcffc1021f904a08e623
Marian Secarea <se>
2018-04-25 21:35:25 +0300

    Works some dropdown

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
     CommonModule,
     PagesRoutingModule,
+    FormsModule
   ],
   declarations: [HomeComponent, NavbarComponent]
 })
